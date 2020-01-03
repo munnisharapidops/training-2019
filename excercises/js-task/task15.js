ed9aedecb02e95c16bdcd38787ca31cabe83a28e
munnisha <munnisha.sheik@rapidops.com>
2020-01-03 19:11:06 +0530

    task15.js added

---
+++ excercises/js-task/task15.js
@@ -0,0 +1,15 @@
+function valid_email(str)
+{
+let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;  
+if(mailformat.test(str))
+{  
+console.log("Valid email address!");  
+}  
+else  
+{  
+console.log("You have entered an invalid email address!");  
+}
+}
+
+valid_email('me-infoample.com');
+
